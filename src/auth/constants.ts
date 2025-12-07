848bbf136b839000980375ba2407454047121a7a
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2025-12-07 18:39:36 +0000

    chore: explicitly type `jwtConstants` as `any`.

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const jwtConstants = {
+export const jwtConstants: any = {
   accessTokenSecret: process.env.JWT_ACCESS_SECRET || process.env.JWT_SECRET,
   refreshTokenSecret:
     process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET + '_refresh',
